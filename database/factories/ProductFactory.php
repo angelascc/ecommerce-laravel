065754850bca051e03454da57d0a867ceeb1cb99
Angela <angelascc16@gmail.com>
2024-06-21 21:49:56 -0500

    new product model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'price' => 200,
+            'description' => $this->fakke()->text($maxNbChars = 200),
+            'thumbnail' => "https://picsum.photos/550/600"
+        ];
+    }
+}
